5725a0bc288cd9224ce8408e997ff2aedbd5b23a
Danilo Assis Nobre dos Santos Silva <danilo@lavid.ufpb.br>
2017-11-15 15:37:21 -0300

    Captistrano deploy user

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server 'ec2-54-187-150-55.us-west-2.compute.amazonaws.com', user: 'ubuntu', roles: %w{app db web}
+server 'ec2-54-187-150-55.us-west-2.compute.amazonaws.com', user: 'deploy', roles: %w{app db web}
 
 
 
